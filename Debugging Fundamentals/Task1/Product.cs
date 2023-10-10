9e401b10fcc7e3935b5b3c4e264a724326b974cc
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-10-10 15:11:27 +0300

    FEATURE: Debugging Fundamentals
    Home Task 2

---
+++ Debugging Fundamentals/Task1/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product : IEquatable<Product>
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public bool Equals(Product product)
+        {
+            return product != null &&
+                     Name == product.Name &&
+                       Price == product.Price;
+        }
+    }
+}
