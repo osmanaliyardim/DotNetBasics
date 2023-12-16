2f70bf25605dc996c364a5684f3d4ea60ed92163
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-12-16 11:35:19 +0300

    Revert "FEATURE: ADO.NET"
    This reverts commit e655d83e39feb4fd93c76865c4d53ead732d0060.

--- ADONETLib/ADONETLib/DBConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ADONETLib;
-
-public static class DBConfig
-{
-    public static string ConnectionString { get; set; } = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AdoNetDB;Integrated Security=True;";
-}
