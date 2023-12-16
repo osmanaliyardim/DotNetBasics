aa67ebceef1d33c69d56c02af8cf84f73d701dde
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-12-16 11:36:48 +0300

    FEATURE: ADO.NET

---
+++ ADONETLib/ADONETLib/DBConfig.cs
@@ -0,0 +1,6 @@
+﻿namespace ADONETLib;
+
+public static class DBConfig
+{
+    public static string ConnectionString { get; set; } = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AdoNetDB;Integrated Security=True;";
+}
